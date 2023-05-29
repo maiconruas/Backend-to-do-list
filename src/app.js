00a6bffe02e68b1d06f8fad905de4a701e628d91
Maicon Ruas <maicon_ruas@hotmail.com>
2023-05-29 14:13:33 -0300

    Criando configuração do banco de dados

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = require('./router');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 
+mongoose.connect(config.connectionString);
 app.use(router);
 
 module.exports = app;
